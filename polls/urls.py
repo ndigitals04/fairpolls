5893efa1ed506faa47ed991c2a7b24607a7609c2
ndigitals <ndigitals001@gmail.com>
2024-05-13 01:14:48 +0100

    Set up view and url to process the vote form, the form now adds selected choice to the database

--- polls/urls.py
+++ polls/urls.py
@@ -5,4 +5,6 @@ app_name= "polls"
 urlpatterns = [
     path('',views.index, name="index"),
     path('<int:question_id>/',views.questionDetail, name="question_detail"),
+    path('<int:question_id>/vote',views.vote, name="vote"),
+    path('<int:question_id>/voted',views.votedSuccess, name="voted_success_page"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.db.models import F
 from .models import Voter,PollVote, Question, Choice
 # Create your views here.
 def index(request):
@@ -15,3 +17,21 @@ def questionDetail(request,question_id):
         "question": question
     }
     return render(request, "polls/question_detail.html", context)
+
+def vote(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except(KeyError,Choice.DoesNotExist):
+        context = {
+            "question":question,
+            "error_message": "You didn't select a choice"
+        }
+        return render(request, "polls/question_detail.html", context)
+        return HttpResponseRedirect(reverse("polls:index"))
+    selected_choice.votes = F("votes") + 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse("polls:voted_success_page", args=(question_id,)))
+
+def votedSuccess(request, question_id):
+    return render(request, "polls/succes_vote.html")
