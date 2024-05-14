4a912c4f99aa7afdd90d883d4ca7d8dce709f16e
ndigitals <ndigitals001@gmail.com>
2024-05-14 08:40:56 +0100

    Configured voting page

--- polls/urls.py
+++ polls/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('<int:question_id>/',views.questionDetail, name="question_detail"),
     path('<int:question_id>/vote',views.vote, name="vote"),
     path('<int:question_id>/voted',views.votedSuccess, name="voted_success_page"),
+    path('<int:question_id>/result',views.result, name="result"),
+    path('login',views.loginView, name="login"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -4,7 +4,11 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.db.models import F
 from django.utils import timezone
 from .models import Voter,PollVote, Question, Choice
+from .forms import CreationUserForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+@login_required(login_url = "polls:login")
 def index(request):
     question_list = Question.objects.all
     context = {
@@ -12,8 +16,15 @@ def index(request):
     }
     return render(request, "polls/index.html", context)
 
+@login_required(login_url = "polls:login")
 def questionDetail(request,question_id):
     question = Question.objects.get(pk=question_id)
+    if question.closed_date < timezone.now():
+        context = {
+            "question":question,
+            "message":"This poll is closed"
+        }
+        return render(request,"polls/question_detail.html", context)
     try:
         poll_voted_by_voter = PollVote.objects.get(question=question, voter=request.user.voter)
         print(poll_voted_by_voter)
@@ -32,6 +43,7 @@ def questionDetail(request,question_id):
         }
         return render(request, "polls/question_detail.html", context)
 
+@login_required(login_url = "polls:login")
 def vote(request,question_id):
     question = get_object_or_404(Question,pk=question_id)
     try:
@@ -52,3 +64,35 @@ def vote(request,question_id):
 
 def votedSuccess(request, question_id):
     return render(request, "polls/succes_vote.html")
+
+@login_required(login_url = "polls:login")
+def result(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    highest_votes = 0
+    choiceId = ""
+    for choice in question.choice_set.all():
+        votes = choice.votes
+        if votes > highest_votes:
+            highest_votes = votes
+            choiceId = choice.id
+    winner = Choice.objects.get(pk=choiceId)
+    context = {
+        "highest_votes":highest_votes,
+        "winner":winner,
+        "question":question
+    }
+    return render(request, "polls/result.html", context)
+
+def loginView(request):
+    form = CreationUserForm()
+    context= {"form":form}
+
+    if request.method == "POST":
+        print(request.POST)
+        voter_id = request.POST.get("voter_id")
+        password = request.POST.get("password")
+        user = authenticate(request, username=voter_id, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("polls:index"))
+    return render(request, "polls/login.html", context)
